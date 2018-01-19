572c10ae999d6db53650891d0decc8190ce6c4cd
tenisistaa <krzychu_ten@o2.pl>
2018-01-19 22:14:04 +0100

    test substraction

--- file.js
+++ file.js
@@ -1,5 +1,15 @@
-module.exports = sum
+module.exports.sum = sum;
+module.exports.product = product;
+module.exports.subtraction = subtraction;
 
 function sum(a, b) {
-  return a + b
+  return a + b;
+}
+
+function product(a, b) {
+  return a * b;
+}
+
+function subtraction(a, b) {
+  return a - b;
 }
--- file.test.js
+++ file.test.js
@@ -1,5 +1,19 @@
-const sum = require('./file')
+const file = require('./file');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1,2)).toBe(3)
+test('adds 1 and 2 to equal 3', () => {
+  expect(file.sum(1, 2)).toBe(3);
+})
+
+describe('Group', () => {
+  test('product 2 and 5 to equal 10', () => {
+    expect(file.product(2, 5)).toBe(10);
+  })
+
+  test('product 3 and 7 to equal 21', () => {
+    expect(file.product(3, 7)).toBe(21);
+  })
+});
+
+test('subtruction 6 and 2 to equal 4', () => {
+  expect(file.subtraction(6,2)).toBe(4);
 })
